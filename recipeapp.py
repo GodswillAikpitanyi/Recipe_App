fe83f669d47a2b78c1842f586a4381976ae08921
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-06-01 11:03:18 +0100

    updates

---
+++ recipeapp.py
@@ -0,0 +1,60 @@
+# Imports #
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+
+# Configurations #
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'eedbf600942368e59f9b73eefa187ba9'
+
+# Posts #
+posts = [
+    {
+        'author': 'Mr Hamilton',
+        'title': 'Initial recipe',
+        'content': 'first post',
+        'date_posted': 'May 30th, 2023'
+    },
+    {
+        'author': 'Miss Kelly Piquet',
+        'title': 'Subsequent recipe',
+        'content': 'second post',
+        'date_posted': 'May 31th, 2023'
+    }
+]
+
+
+# Routes #
+@app.route("/")
+@app.route("/home")
+def home():
+    return (render_template('home.html', posts=posts))
+
+@app.route("/about")
+def about():
+    return (render_template('about.html', title='About'))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return (render_template('register.html', title='Register', form=form))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
+    return (render_template('login.html', title='Login', form=form))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
